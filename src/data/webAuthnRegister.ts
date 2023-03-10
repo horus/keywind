eb7e840441ff79c7d5adb7e5f422c02da0f6494c
horus <horus.li@gmail.com>
2023-03-10 11:02:13 +0800

    Fix: label prompt not properly pops up
    In Chrome 111.0.5563.64 & Safari 16.2 (18614.3.7.1.5) on macOS, during
    webauthn register process it won't display a prompt asking for the label.
    
    Various solutions were tried, including put the prompt before the if block
    contains getTransports(), which worked. Although the cause has not yet been
    fully identified, I rewrote the code consulting the official implementation.
    
    P./S. with firefox 111.0b8 everything worked as expected.

--- src/data/webAuthnRegister.ts
+++ src/data/webAuthnRegister.ts
@@ -171,10 +171,7 @@ document.addEventListener('alpine:init', () => {
               result instanceof PublicKeyCredential &&
               result.response instanceof AuthenticatorAttestationResponse
             ) {
-              const { getTransports } = result.response;
-
               window.result = result;
-
               const publicKeyCredentialId = result.rawId;
 
               attestationObjectInput.value = base64url.stringify(
@@ -192,9 +189,8 @@ document.addEventListener('alpine:init', () => {
                 { pad: false }
               );
 
-              if (typeof getTransports === 'function') {
-                const transports = getTransports();
-
+              if (typeof result.response.getTransports === 'function') {
+                const transports = result.response.getTransports();
                 if (transports) {
                   transportsInput.value = getTransportsAsString(transports);
                 }
